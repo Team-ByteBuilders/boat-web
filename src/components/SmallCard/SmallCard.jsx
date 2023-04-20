7f6cc0c7e892d5184823be7d712e95fc69853255
Tanishq <tanishqjain1652003@gmail.com>
2023-04-21 01:03:32 +0530

    UI improvized and search functionality added

--- src/components/SmallCard/SmallCard.jsx
+++ src/components/SmallCard/SmallCard.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './SmallCard.css'
+import { useNavigate } from 'react-router'
 
 function SmallCard(props) {
+    const navigate = useNavigate();
+    const handleclick = () => {
+        navigate(`/bigcard/${props.name}`, { state: { image: props.image, name: props.name, details: props.details } })
+    }
     return (
-        <div className='smallCard'>
-            kuch toh h
+        <div className='smallCard' onClick={handleclick}>
+            <img src={props.image} alt="haha" />
+            <div className="smallCardP"><p>{props.name}</p></div>
         </div>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import "./Home.css"
 import SmallCard from '../../components/SmallCard/SmallCard'
+// import axios from 'axios';
+import loading from "../../assets/images/loading-gif.gif"
+import {  useSelector } from 'react-redux';
 
 function Home() {
+    let data = useSelector(state => state.monuments)
     return (
         <div className='home'>
             <Navbar />
             <div className="nearbyDiv">
                 <h1 className='nearbyHeading'>Nearby Monument</h1>
                 <div className="nearbyCards">
-                        <SmallCard />
-                        <SmallCard />
-                        <SmallCard />
-                        <SmallCard />
-                        <SmallCard />
+                    {
+                        data.length === 0 && <img className='loading' src={loading} alt="" />
+                    }
+                    {
+                        data && data.length > 0 && data.map((item, index) => {
+                            return (
+                                <SmallCard key={index} name={item.name} image={item.image} details={item.details} />
+                            )
+                        })
+                    }
                 </div>
             </div>
 
